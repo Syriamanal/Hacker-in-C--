e97ca7eb7c3cc2d6978c6ab518a94d8e82ecb21b
Syriamanal <Shadinachat123@gmail.com>
2013-12-27 20:16:54 -0800

    Create Hacker in C++.cpp

---
+++ Hacker in C++.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std:
+
+int main()
+{
+int UserName;
+int PassWord
+
+
+cout << "Enter your user name ...";
+cout << endl;
+cin >> UserName;
+cout << "Enter your password ...";
+cout << endl;
+cin >> PassWord;
+cout << "protecting your data ..."
+cout << endl;
+cout << "Data protected ...";
+
+}
